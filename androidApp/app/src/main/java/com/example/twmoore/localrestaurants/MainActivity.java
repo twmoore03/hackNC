d2b96898af99705b37f1d970ba372e8e24de7bef
lackeyw <lackeyw@dhcp-v880-14-01006.1Xwireless.unc.edu>
2016-10-29 11:50:54 -0400

    added Google Play API

--- androidApp/app/src/main/java/com/example/twmoore/localrestaurants/MainActivity.java
+++ androidApp/app/src/main/java/com/example/twmoore/localrestaurants/MainActivity.java
@@ -9,9 +9,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (mGoogleApiClient == null) {         //adds the Google Play API
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
     protected void checkPermissions() {
         
+    }
+
+    protected void onStart(){
+
+        mGoogleApiClient.connect();             //connects to Google Play services
+        super.onStart();
+    }
+
+    protected void onPause(){
+
+    }
+
+    protected void onDestroy(){
+
+    }
+
+    protected void onResume(){
+
+    }
+
+    protected void onStop(){
+
+        mGoogleApiClient.disconnect();          //disconnects from Google Play services
+        super.onStop();
+
+    }
+
+    protected void onRestart(){
+
     }
 }
