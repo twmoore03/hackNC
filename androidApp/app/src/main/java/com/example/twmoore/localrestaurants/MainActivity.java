fb55ff3745b39aea07a23dfa5040840069769e42
twmoore03 <M01109087t!>
2016-10-29 17:00:42 -0400

    added missing brackets

--- androidApp/app/src/main/java/com/example/twmoore/localrestaurants/MainActivity.java
+++ androidApp/app/src/main/java/com/example/twmoore/localrestaurants/MainActivity.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -14,7 +13,8 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
 import java.io.IOException;
-import java.net.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
@@ -37,38 +37,40 @@ public class MainActivity extends AppCompatActivity implements
                     .addApi(LocationServices.API)
                     .build();
         }
+
     }
 
-    protected void onStart(){
+    protected void onStart() {
 
         mGoogleApiClient.connect();             //connects to Google Play services
         super.onStart();
-    }
-
-    protected void onPause(){
 
     }
 
-    protected void onDestroy(){
+    protected void onPause() {
 
     }
 
-    protected void onResume(){
+    protected void onDestroy() {
 
     }
 
-    protected void onStop(){
+    protected void onResume() {
+
+    }
+
+    protected void onStop() {
 
         mGoogleApiClient.disconnect();          //disconnects from Google Play services
         super.onStop();
 
     }
 
-    protected void onRestart(){
+    protected void onRestart() {
 
     }
 
-    protected double[] getLocation(){
+    protected double[] getLocation() {
         return latitudeLongitude;
     }
 
@@ -80,7 +82,7 @@ public class MainActivity extends AppCompatActivity implements
         return latitudeLongitude[1];
     }
 
-    protected Object getNearbyPlace(){
+    protected Object getNearbyPlace() {
         StringBuilder url = new StringBuilder("//maps.googleapis.com/maps/api/place/nearbysearch/json?");
         Object output;
 
@@ -99,55 +101,55 @@ public class MainActivity extends AppCompatActivity implements
         String urlString = url.toString();
 
         //connection
-        try{
-        URL connection = new URL(urlString);
-            output = connection.getContent();}
-        catch (MalformedURLException e){
+        output = null;
+        try {
+            URL connection = new URL(urlString);
+            output = connection.getContent();
+        } catch (MalformedURLException e) {
             System.out.println("URL failure");
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Input Output Error");
         }
 
 
         return output;
+    }
 
 
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        //need to add a checkPermission method
+        @Override
+        public void onConnected (Bundle connectionHint){
+            //need to add a checkPermission method
 
-        int permissionCheck = ContextCompat.checkSelfPermission(this,
-                ACCESS_FINE_LOCATION);
+            int permissionCheck = ContextCompat.checkSelfPermission(this,
+                    ACCESS_FINE_LOCATION);
 
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
 
-                //Show an explanation to the user. After the user sees the explanation, try again to request permission
-            } else {
-                //need to define an int for this later on for mypermissions request
+                    //Show an explanation to the user. After the user sees the explanation, try again to request permission
+                } else {
+                    //need to define an int for this later on for mypermissions request
                /*
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                         MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
                         */
-            }
-        } else {
-            Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            //need to define an xmlString for that I think
-            if (mLastLocation != null) {
-                latitudeLongitude[0] = mLastLocation.getLatitude();
-                latitudeLongitude[1] = mLastLocation.getLongitude();
                 }
+            } else {
+                Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                //need to define an xmlString for that I think
+                if (mLastLocation != null) {
+                    latitudeLongitude[0] = mLastLocation.getLatitude();
+                    latitudeLongitude[1] = mLastLocation.getLongitude();
+                }
+            }
+        }
+        @Override
+        public void onConnectionSuspended ( int i){
+
+        }
+
+        @Override
+        public void onConnectionFailed (ConnectionResult connectionResult){
+
         }
     }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-}
